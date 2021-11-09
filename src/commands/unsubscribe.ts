61e29f46302d578ffbad3ecc66eaad6b2e6d2749
Guill3okk <91464453+Guill3okk@users.noreply.github.com>
2021-11-09 03:51:37 +0100

    Update unsubscribe.ts

--- src/commands/unsubscribe.ts
+++ src/commands/unsubscribe.ts
@@ -2,12 +2,12 @@ import Command from '../lib/structures/Command';
 
 export default new Command('unsubscribe', async (caller, cmd, log) => {
 	if (!log!.subscriptions.includes(cmd.msg.author.id))
-		return caller.utils.discord.createMessage(cmd.channel.id, 'You are not subscribed to this thread.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'No estás suscrito a este hilo.');
 	const updated = await caller.db.updateLog(log!._id, 'subscriptions', cmd.msg.author.id, 'PULL');
 	if (updated)
-		return caller.utils.discord.createMessage(cmd.channel.id, 'You will not receive any longer notifications on new responses.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'No recibirás más notificaciones sobre nuevas respuestas.');
 	if (!updated)
-		return caller.utils.discord.createMessage(cmd.channel.id, 'There was an error unsubscribing you to this thread.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'Hubo un error al cancelar tu suscripción a este hilo.');
 },
 {
 	level: 'SUPPORT',
