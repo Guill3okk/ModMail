12a2eed7fb4cd7070627274e8a7f265fb79a500a
Guill3okk <91464453+Guill3okk@users.noreply.github.com>
2021-11-09 04:28:30 +0100

    Update rename.ts

--- src/commands/rename.ts
+++ src/commands/rename.ts
@@ -1,12 +1,12 @@
 import Command from '../lib/structures/Command';
 
 export default new Command('rename', async (caller, cmd) => {
-	if (!cmd.args[0]) return caller.utils.discord.createMessage(cmd.channel.id, 'You have to provide a new channel name.');
+	if (!cmd.args[0]) return caller.utils.discord.createMessage(cmd.channel.id, 'Debes proporcionar un nuevo nombre de canal.');
 	cmd.channel.edit({
 		name: cmd.args.join()
 	})
-		.then(() => caller.utils.discord.createMessage(cmd.channel.id, 'The name has been edited.'))
-		.catch(() => caller.utils.discord.createMessage(cmd.channel.id, 'There has been an error editing the name of the channel.'));
+		.then(() => caller.utils.discord.createMessage(cmd.channel.id, 'El nombre ha sido editado.'))
+		.catch(() => caller.utils.discord.createMessage(cmd.channel.id, 'Ha habido un error al editar el nombre del canal.'));
 },
 {
 	level: 'ADMIN',
