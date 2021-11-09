893583033cc5012fe07fec6bb50666b1f2b95ed4
Guill3okk <91464453+Guill3okk@users.noreply.github.com>
2021-11-09 03:55:07 +0100

    Update title.ts

--- src/commands/title.ts
+++ src/commands/title.ts
@@ -2,12 +2,12 @@ import Command from '../lib/structures/Command';
 
 export default new Command('title', async (caller, cmd, log) => {
 	if (!cmd.args[0])
-		return caller.utils.discord.createMessage(cmd.channel.id, 'Provide a title or write `removetitle` to remove the title.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'Proporcione un título o escriba `removetitle` para quitar el título.');
 	const updated = await caller.db.updateLog(log!._id, 'title', cmd.args[0] === 'removetitle' ? '' : cmd.args.join(' '), cmd.args[0] === 'removetitle' ? 'UNSET' : 'SET');
 	if (updated)
-		return caller.utils.discord.createMessage(cmd.channel.id, 'The title has been updated.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'El título se ha actualizado.');
 	if (!updated)
-		return caller.utils.discord.createMessage(cmd.channel.id, 'The title could not be updated.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'No se pudo actualizar el título.');
 },
 {
 	level: 'SUPPORT',
