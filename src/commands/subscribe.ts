d925cf805c37c40ee5a51cb2d4850bbc28ca128a
Guill3okk <91464453+Guill3okk@users.noreply.github.com>
2021-11-09 03:41:31 +0100

    Update subscribe.ts

--- src/commands/subscribe.ts
+++ src/commands/subscribe.ts
@@ -2,12 +2,12 @@ import Command from '../lib/structures/Command';
 
 export default new Command('subscribe', async (caller, cmd, log) => {
 	if (log!.subscriptions.includes(cmd.msg.author.id))
-		return caller.utils.discord.createMessage(cmd.channel.id, 'You are already subscribed to this thread.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'Ya estás suscrito a este hilo.');
 	const updated = await caller.db.updateLog(log!._id, 'subscriptions', cmd.msg.author.id, 'PUSH');
 	if (updated)
-		return caller.utils.discord.createMessage(cmd.channel.id, 'You will now receive notifications on new responses.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'Ahora recibirá notificaciones sobre nuevas respuestas.');
 	if (!updated)
-		return caller.utils.discord.createMessage(cmd.channel.id, 'There was an error subscribing you to this thread.');
+		return caller.utils.discord.createMessage(cmd.channel.id, 'Hubo un error al suscribirte a este hilo.');
 },
 {
 	level: 'SUPPORT',
